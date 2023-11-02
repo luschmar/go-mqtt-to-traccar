429b7f0c466783175b93f042e4d4d24e03684610
luschmar <90399580+luschmar@users.noreply.github.com>
2023-11-02 14:42:09 +0100

    ✅ add tests

--- go-mqtt-to-traccar_test.go
+++ go-mqtt-to-traccar_test.go
@@ -1,13 +1,37 @@
 package main
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 // TestHelloName calls greetings.Hello with a name, checking
 // for a valid return value.
-func TestToParameterList(t *testing.T) {
+func Test_ttnDataToUrl_empty(t *testing.T) {
 	data := TTNData{EndDeviceIds{}, UplinkMessage{}, ""}
 
-	ttnDataToUrl(data)
+	result := ttnDataToUrl(data, "aa/a/aa/aa")
+	assert.Equal(t, "http://10.0.0.10:3055/?id=&lat=0.000000&lon=0.000000&hdop=0.000000&batt=10&md=", result)
+}
+
+func Test_ttnDataToUrl_data(t *testing.T) {
+	decoded := DecodedPayload{1.0, 2.0, 3.0, 4.0, 5.0, "FALSE", "A", nil}
+	uplink := UplinkMessage{decoded}
+	data := TTNData{EndDeviceIds{}, uplink, ""}
+
+	result := ttnDataToUrl(data, "a/b/c/d")
+	assert.Equal(t, "http://10.0.0.10:3055/?id=&lat=1.000000&lon=2.000000&hdop=3.000000&batt=70&md=A", result)
+}
+
+func Test_ttnDataToUrl_t1000(t *testing.T) {
+	messages := make([]Message, 2)
+	messages[0] = Message{"4198", []byte("10"), "Lat"}
+	messages[1] = Message{"4197", []byte("20"), "Long"}
+
+	decoded := DecodedPayload{0.0, 0.0, 0.0, 0.0, 0.0, "FALSE", "A", messages}
+	uplink := UplinkMessage{decoded}
+	data := TTNData{EndDeviceIds{}, uplink, ""}
+
+	result := ttnDataToUrl(data, "a/b/c/d")
+	assert.Equal(t, "http://10.0.0.10:3055/?id=c&lat=10&lon=20&batt=&timestamp=", result)
 }
--- go.mod
+++ go.mod
@@ -2,10 +2,16 @@ module luschmar/go-mqtt-to-traccar
 
 go 1.21
 
-require github.com/eclipse/paho.mqtt.golang v1.4.3
+require (
+	github.com/eclipse/paho.mqtt.golang v1.4.3
+	github.com/stretchr/testify v1.8.4
+)
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sync v0.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
